7379f4f00c2ade624237e90ba470332986065d92
Jose Alfredo <jose.nunez181930@potros.itson.edu.mx>
2023-11-05 14:50:21 -0700

    fix: firts components

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,24 +1,56 @@
+import createTracker, { setupSelect } from './components/createTracker';
+import selectChart from './components/selectChart';
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './selector.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+<!-- USAGE -->
+${createTracker()}
+<br>
+${selectChart()}
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// test 
+const input = document.querySelector<HTMLTextAreaElement>('.text-input')
+const button = document.querySelector<HTMLButtonElement>('#btn-create-tracker')
+button!.addEventListener('click', () => {
+
+  //TODO: implement same functionality as enter input
+});
+input!.addEventListener('keypress', (e) => {
+
+  if (e.key === 'Enter') {
+    e.preventDefault()
+    const typeTracker = document.querySelector<HTMLInputElement>('.select-value');
+    const body = {
+      name: input!.value,
+      typeTracker: typeTracker!.value
+    }
+    console.log(JSON.stringify(body))
+    //TODO:: fetch
+    fetch("http://localhost:3000/account/tracker/6545805733a99e070b50b1c6", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-auth-token': '<token>'
+      },
+      body: JSON.stringify(body)
+    })
+      .then(res => {
+        if (res.ok) {
+          input!.value = ""
+          return res.json()
+        }
+        throw new Error("Error")
+
+      })
+      .then(res => console.log(res))
+      .catch(err => alert(err.message))
+  }
+});
+
+
+
+//setup selects
+setupSelect(document.querySelector<HTMLDivElement>('#select-type-tracker')!)
+setupSelect(document.querySelector<HTMLDivElement>('#select-type-graphic')!)
