34fe3e60b8eeca23b7578bdd3aadb24482a39ef5
Jose Alfredo <jose.nunez181930@potros.itson.edu.mx>
2023-11-20 23:59:04 -0700

    fix: remove implementations of mytrackers component

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,20 +1,9 @@
-import createTracker from './components/mytrackers/createTracker.component';
 import './style.css'
 import './selector.css'
 import darkModeChange, { setupTheme } from './components/buttonDark.component';
-import { myTrackersComponent } from './components/mytrackers/myTrackers.component';
-import createView from './components/mytrackers/tracker.view';
 import { setupRouting } from './routing';
 
-
 document.body.appendChild(darkModeChange());
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-
-const trackers = createView(app)
-createTracker(trackers);
-myTrackersComponent(trackers, [{ name: 'Tracker 1' }, { name: 'Tracker 2' }, { name: 'Tracker 3' }]);
-
 
 setupRouting()
 setupTheme()
